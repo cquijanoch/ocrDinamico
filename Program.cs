5ce7e6adcb398b305c215f03ac16bc893cfae2aa
tharmathozz <cquijanochavez@gmail.com>
2016-10-26 20:39:52 -0500

    probando

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace Gestures
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
